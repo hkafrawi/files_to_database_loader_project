7beebc31a3897ee6b05ff4da519a748032389d1f
Kafrawi <hassaballaelkafrawi@gmail.com>
2025-01-30 17:52:21 +0100

    Add: adding process_files function
    - Transfered ipython script for clearing database to seperate file clear_db
    - Added necessary imports for app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import configparser
 from IPython import get_ipython
 import pandas as pd
 import json
+import re
+import sys
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -20,6 +22,7 @@ def read_csv(file, schemas, chunksize=10_000):
     ds_name = file_path_list[-2]
     columns = get_column_names(schemas, ds_name)
     df_reader = pd.read_csv(file, names=columns, chunksize=chunksize)
+
 def to_sql(df, db_conn_url, ds_name):
     df.to_sql(
         ds_name,
@@ -27,6 +30,7 @@ def to_sql(df, db_conn_url, ds_name):
         if_exists="append",
         index=False
     )
+
 def db_loader(src_base_dir, db_conn_url, ds_name):
     schemas = json.load(open(f'{src_base_dir}\\schemas.json'))
     files = glob.glob(f"{src_base_dir}\\{ds_name}\\part-*")
@@ -39,43 +43,39 @@ def db_loader(src_base_dir, db_conn_url, ds_name):
             print(f"Populating chunk {i} of {ds_name}")
             to_sql(df,db_conn_url, ds_name)
 
-schemas = json.load(open("data\\retail_db\\schemas.json"))
-columns = get_column_names(schemas=schemas,ds_name='orders')
+def process_files(ds_names=None):
+    database = os.environ.get("DB_TYPE")
+    username = os.environ.get("DB_USER")
+    password = os.environ.get("DB_PASSWORD")
+    host = os.environ.get("DB_HOST")
+    portnumber = os.environ.get("DB_PORT")
+    db_name = os.environ.get("DB_NAME")
 
-df = pd.read_csv(
-    "data\\retail_db\\orders\\part-00000",
-    names=columns
-)
+    conn_url = f"{database}://{username}:{password}@{host}:{portnumber}/{db_name}"
 
-db_config = config["database"]
-database = db_config.get("database")
-username = db_config.get("username")
-password = db_config.get("password")
-server = db_config.get("server")
-portnumber = db_config.getint("portnumber")  
-db_name = db_config.get("db_name")
+    src_base_dir = os.environ.get("SRC_BASE_DIR")
 
-conn_url = f"{database}://{username}:{password}@localhost:{portnumber}/{db_name}"
+    schemas = json.load(open(f"{src_base_dir}\\schemas.json"))
+
+    if not ds_names:
+        ds_names = schemas.key()
+
+    for ds_name in ds_names:
+        try:
+            print(f"Processing {ds_name}")
+            db_loader(src_base_dir, conn_url, ds_name)
+        except NameError as e:
+            print(e)
+            pass
+        except Exception as e:
+            print(e)
+            pass
+        
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        ds_names = json.loads(sys.argv[1])
+        process_files(ds_names)
+    else:
+        process_files()
 
 
-# os.environ.update({'DB_HOST': 'localhost'})
-# ipython = get_ipython()
-
-# ipython.run_line_magic("env","DB_HOST=localhost")
-# ipython.run_line_magic("load_ext","sql")
-# ipython.run_line_magic("config","SqlMagic.style = \'_DEPRECATED_DEFAULT\'")
-# ipython.run_line_magic("env",f"DATABASE_URL={conn_url}")
-# print(ipython.run_line_magic("sql","SELECT * FROM orders LIMIT 10;"))
-# ipython.run_line_magic("sql","TRUNCATE TABLE orders")
-# print(os.environ.get("DB_HOST"))
-# query = "SELECT * FROM orders Limit 10;"
-# print(pd.read_sql(query,conn_url))
-
-df.to_sql(
-    "orders",
-    conn_url,
-    if_exists="replace",
-    index=False
-)
-
-print(pd.read_sql('orders', conn_url))
---
+++ clear_db.py
@@ -0,0 +1,19 @@
+import os
+from IPython import get_ipython
+
+ipython = get_ipython()
+
+database = os.environ.get("DB_TYPE")
+username = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASSWORD")
+host = os.environ.get("DB_HOST")
+portnumber = os.environ.get("DB_PORT")
+db_name = os.environ.get("DB_NAME")
+
+conn_url = f"{database}://{username}:{password}@{host}:{portnumber}/{db_name}"
+
+ipython.run_line_magic("load_ext","sql")
+ipython.run_line_magic("config","SqlMagic.style = \'_DEPRECATED_DEFAULT\'")
+ipython.run_line_magic("env",f"DATABASE_URL={conn_url}")
+ipython.run_line_magic("sql","TRUNCATE TABLE orders")
+
