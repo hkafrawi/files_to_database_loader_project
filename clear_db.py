48045a65728d59fefb280e331d81746bc081abc1
Kafrawi <hassaballaelkafrawi@gmail.com>
2025-01-31 20:09:54 +0100

    Refactor: improving readbility for clear_db.py

--- clear_db.py
+++ clear_db.py
@@ -18,6 +18,12 @@ conn_url = f"{database}://{username}:{password}@{host}:{portnumber}/{db_name}"
 ipython.run_line_magic("load_ext","sql")
 ipython.run_line_magic("config","SqlMagic.style = \'_DEPRECATED_DEFAULT\'")
 ipython.run_line_magic("env",f"DATABASE_URL={conn_url}")
-ipython.run_line_magic("sql","TRUNCATE TABLE orders, categories, customers, departments, order_items, products")
-ipython.run_line_magic("sql","SELECT * FROM products LIMIT 10")
+#ipython.run_line_magic("sql","TRUNCATE TABLE orders, categories, customers, departments, order_items, products")
+print(f"Orders: \n{ipython.run_line_magic('sql','SELECT count(*) FROM orders')}")
+print(f"categories: \n{ipython.run_line_magic('sql','SELECT count(*) FROM categories')}")
+print(f"customers: \n{ipython.run_line_magic('sql','SELECT count(*) FROM customers')}")
+print(f"departments: \n{ipython.run_line_magic('sql','SELECT count(*) FROM departments')}")
+print(f"order_items: \n{ipython.run_line_magic('sql','SELECT count(*) FROM order_items')}")
+print(f"products: \n{ipython.run_line_magic('sql','SELECT count(*) FROM products')}")
+
 
